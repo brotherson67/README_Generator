52794703972447d4fcc08c3d1376dae09f0395e8
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-07 21:10:41 -0700

    made answers display

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
+const { title } = require('process');
 
 // TODO: Create an array of questions for user input
 const questions = [
@@ -56,56 +57,57 @@ const questions = [
     }
     
 ];
-
-inquirer
-    .prompt(questions)
-    .then(console.log(answers))
+test = () => {
+    inquirer
+        .prompt(questions)
+        .then(answers => console.lof(answers));
+}
 
 
 
 // TODO: Create a function to write README file
-// function writeToFile(fileName, data) {
-//     return `
-//         # ${fileName}
+function writeToFile(fileName, data) {
+    return `
+        # ${fileName}
 
-//         ## Description
-//         This is where the basic README stuff will go
+        ## Description
+        This is where the basic README stuff will go
 
-//         ## Table of Contents
+        ## Table of Contents
 
-//         ================================================
+        ================================================
 
-//         ## Usage
+        ## Usage
 
-//         ================================================
+        ================================================
 
-//         ##License
+        ##License
 
-//         ================================================
+        ================================================
 
-//         ## Contributors 
+        ## Contributors 
 
-//         ================================================
+        ================================================
 
-//         ## tests 
+        ## tests 
 
-//         ================================================
+        ================================================
 
-//         ## Questions 
-//         If you have any questions please email me at 
-//         My GitHub account is ${github} you'll be able to find more there
-//         ================================================
-//         `
-// }
+        ## Questions 
+        If you have any questions please email me at 
+        My GitHub account is ${github} you'll be able to find more there
+        ================================================
+        `
+}
 
-// // TODO: Create a function to initialize app
-// function init() {
-//     fs.writeFile(`${fileName}.md`, writeToFile(fileName, data), err => {
-//         if (err) throw new Error(err);
+// TODO: Create a function to initialize app
+function init() {
+    fs.writeFile(`${fileName}.md`, writeToFile(fileName), err => {
+        if (err) throw new Error(err);
 
-//         console.log('Your README file has been generated');
-//     })
-// }
+        console.log('Your README file has been generated');
+    })
+}
 
-// // Function call to initialize app
-// init();
+// Function call to initialize app
+init();
