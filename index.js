6a9a7222361d2cce3dfd598baee85bf6a7bcbdec
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-07 19:30:09 -0700

    filled questions array

--- index.js
+++ index.js
@@ -3,7 +3,54 @@ const inquirer = require('inquirer');
 const fs = require('fs');
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What would you like to call your project?'
+    },
+    {  
+        type: 'input',
+        name: 'sections',
+        message: 'What are the sections of your project?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Enter a description of your project'
+    },
+    {
+        type: 'input',
+        name: 'TofC',
+        message: 'Enter a table of contents'
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What will your program be used for?'
+    },
+    {
+        type: 'input',
+        name: 'license',
+        message: 'What kind of license does your project have'
+    },
+    {
+        type: 'input',
+        name: 'contributing',
+        message: 'Who contributed to your project?'
+    },
+    {
+        type: 'input',
+        name: 'tests',
+        message: 'Enter one or more sample tests for people to try out'
+    },
+    {
+        type: 'input',
+        name: 'questions',
+        message: 'enter your github name and email address for people to ask you questions'
+    }
+    
+];
 
 
 
