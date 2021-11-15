aa1a4d7ad34bfeea33078871e010cb89aa23c3c4
brotherson67 <mitchell.brotherson@gmail.com>
2021-11-14 18:23:56 -0700

    added original readme

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 // TODO: Include packages needed for this application
 const inquirer = require('inquirer');
 const fs = require('fs');
-const generate = ('./generateMarkdown.js')
+const generate = ('./generateMarkdown')
 
 // TODO: Create an array of questions for user input
 const questions = [
